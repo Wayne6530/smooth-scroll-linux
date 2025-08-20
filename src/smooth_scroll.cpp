783fce2bec402166ebf94191443c4c33c1032a4f
Wayne <759858299@qq.com>
2025-08-20 14:00:47 +0800

    1. feat: support deb package generation

--- src/smooth_scroll.cpp
+++ src/smooth_scroll.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Wayne6530
+
 #include <atomic>
 #include <optional>
 #include <chrono>
--- src/wheel_smoother.cpp
+++ src/wheel_smoother.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Wayne6530
+
 #include "wheel_smoother.h"
 
 #include <spdlog/spdlog.h>
